a60c6ea6d4631da97ee3c9ad9e8ad4cd4ad154e0
estagiario <otaviocha@gmail.com>
2019-03-07 15:05:27 -0300

    [CHANGES] - Menu colapsando
    Agora o menu colapsa da maneira certa, falta o estilo para funcionar devidamente

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
-$("#mainHeader").addClass('header-background');
+(function () {
+
+	$("#mainHeader").addClass('header-background');
+
+	document.getElementById("open-nav").addEventListener("click", function (event) {
+		event.preventDefault();
+		document.body.classList.toggle("nav-open");
+	});
+	
+})();
